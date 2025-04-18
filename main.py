7a0ae3db15abf2ac4068c7e94afd60e5fa76e752
devplayground00 <devplayground00@gmail.com>
2025-04-18 23:53:01 +0800

    remove test 2

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Program:
         self.email_pass = email_pass
 
     async def run_async(self):
-        working_folder = r"test2"
+        working_folder = r""
         history_folder = r""
         fault_folder = r""
         pdf_text = extract_text_from_pdf(working_folder)
